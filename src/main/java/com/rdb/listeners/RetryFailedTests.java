dd4c662262e3133c02ecba4072424a3b9c7a8e74
Rinaldo23 <rinaldobadigar@gmail.com>
2025-02-08 16:06:46 +0530

    Added configurable toggle for retrying failed tests using properties file

--- src/main/java/com/rdb/listeners/RetryFailedTests.java
+++ src/main/java/com/rdb/listeners/RetryFailedTests.java
@@ -1,19 +1,26 @@
 package com.rdb.listeners;
 
+import com.rdb.enums.ConfigProperties;
+import com.rdb.utils.PropertyUtils;
 import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
 
 public final class RetryFailedTests implements IRetryAnalyzer {
 
 
+    private final int retries = 1;
     private int count = 0;
-    private int retries = 1;
 
     @Override
     public boolean retry(ITestResult result) {
-        if (count < retries) {
-            count++;
-            return true;
+        try {
+            if (PropertyUtils.getValue(ConfigProperties.RETRYFAILEDTESTS).equalsIgnoreCase("yes") && count < retries) {
+                count++;
+                return true;
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
         return false;
     }
