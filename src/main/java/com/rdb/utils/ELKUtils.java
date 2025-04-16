987acfa161df75a34261c48789b7d98fa5542bfd
Rinaldo23 <rinaldobadigar@gmail.com>
2025-04-16 21:16:21 +0530

    Created a config switch to send test logs to ELK Dashboard

--- src/main/java/com/rdb/utils/ELKUtils.java
+++ src/main/java/com/rdb/utils/ELKUtils.java
@@ -1,5 +1,6 @@
 package com.rdb.utils;
 
+import com.rdb.enums.ConfigProperties;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.testng.Assert;
@@ -11,42 +12,48 @@ import static io.restassured.RestAssured.given;
 
 public final class ELKUtils {
 
+    private static final String SENDLOGSTOELK = PropertyUtils.getValue(ConfigProperties.SENDLOGSTOELK);
+
     private ELKUtils() {
     }
 
     public static void sendDetailsToElk(String testname, String status) {
-        HashMap<String, String> map = new HashMap<>();
-        map.put("testName", testname);
-        map.put("status", status);
-        map.put("executionTime", LocalDateTime.now().toString());
+        if (SENDLOGSTOELK.equalsIgnoreCase("yes")) {
+            HashMap<String, String> map = new HashMap<>();
+            map.put("testName", testname);
+            map.put("status", status);
+            map.put("executionTime", LocalDateTime.now().toString());
 
-        Response response = given()
-                .log().all()
-                .contentType(ContentType.JSON)
-                .body(map)
-                .post("http://localhost:9200/selenium-automation-logs/_doc");
+            Response response = given()
+                    .log().all()
+                    .contentType(ContentType.JSON)
+                    .body(map)
+                    .post("http://localhost:9200/selenium-automation-logs/_doc");
 
-        Assert.assertEquals(response.statusCode(), 201);
-        response.prettyPrint();
+            Assert.assertEquals(response.statusCode(), 201);
+            response.prettyPrint();
+        }
     }
 
     public static void deleteAllPreviousLogsFromIndex() {
-        String payload = """
-                {
-                    "query": {
-                        "match_all": {}
+        if (SENDLOGSTOELK.equalsIgnoreCase("yes")) {
+            String payload = """
+                    {
+                        "query": {
+                            "match_all": {}
+                        }
                     }
-                }
-                """;
+                    """;
 
-        Response response = given()
-                .log().all()
-                .contentType(ContentType.JSON)
-                .body(payload)
-                .post("http://localhost:9200/selenium-automation-logs/_delete_by_query");
+            Response response = given()
+                    .log().all()
+                    .contentType(ContentType.JSON)
+                    .body(payload)
+                    .post("http://localhost:9200/selenium-automation-logs/_delete_by_query");
 
-        Assert.assertEquals(response.statusCode(), 200);
-        response.prettyPrint();
+            Assert.assertEquals(response.statusCode(), 200);
+            response.prettyPrint();
+        }
     }
 
 }
